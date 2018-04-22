e7613b915521bea24ed88af392ff99edbdf722e0
niqdev <niqdev@gmail.com>
2018-04-22 07:26:59 +0100

    add akka tests

---
+++ src/main/g8/app/src/test/scala/$package$/config/SettingsSpec.scala
@@ -0,0 +1,40 @@
+package $package$
+package config
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import org.scalatest.{Matchers, WordSpecLike}
+
+class SettingsSpec extends TestKit(ActorSystem("settings-test")) with WordSpecLike with Matchers {
+
+  val settings = Settings(system)
+
+  object ConfigTest {
+    private[this] val applicationConfig = system.settings.config getConfig "application"
+    private[this] val logConfig = applicationConfig getConfig "log"
+    //private[this] val dockerConfig = applicationConfig getConfig "docker"
+
+    val applicationName: String = applicationConfig getString "name"
+    val logName: String = logConfig getString "name"
+    //val dockerPort: Int = dockerConfig getInt "port"
+  }
+
+  "application name" should {
+    "be equal to $name$" in {
+      ConfigTest.applicationName shouldEqual "$name$"
+    }
+  }
+
+  "log file name" should {
+    "be equal to application name" in {
+      ConfigTest.logName shouldEqual ConfigTest.applicationName
+    }
+  }
+
+//  "exposed docker port" should {
+//    "be equal to application http port" in {
+//      ConfigTest.dockerPort shouldEqual settings.Http.port
+//    }
+//  }
+
+}
---
+++ src/main/g8/app/src/test/scala/$package$/http/RoutesSpec.scala
@@ -0,0 +1,24 @@
+package $package$
+package http
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{Matchers, WordSpec}
+import $package$.Main.routes
+import $package$.http.route.Status
+
+// scalastyle:off underscore.import
+import io.circe.generic.auto._
+import io.circe.syntax._
+// scalastyle:on underscore.import
+
+class RoutesSpec extends WordSpec with Matchers with ScalatestRouteTest {
+
+  "routes /status" should {
+    "return OK for GET requests to the root path" in {
+      Get("/status") ~> routes ~> check {
+        responseAs[String] shouldEqual Status("OK").asJson.noSpaces
+      }
+    }
+  }
+
+}
